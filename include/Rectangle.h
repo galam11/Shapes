ac27514f6a3d694425af587898dc0fe731c02ebb
Gal Amar <gal143628@gmail.com>
2025-11-11 17:12:33 +0200

    fixed all the .h files

--- include/Rectangle.h
+++ include/Rectangle.h
@@ -3,37 +3,33 @@
 #include "Vertex.h"
 #include "Board.h"
 
-class Rectangle {
+class Rectangle
+{
+    // Private members
+    Vertex m_bottomLeft = Vertex(20, 10);
+    Vertex m_topRight = Vertex(30, 20);
 
-private: // Private members
-
-  Vertex m_bottomLeft = Vertex(20 ,10);
-  Vertex m_topRight = Vertex(30, 20);
+    // Rectangle private utility function
+    bool validRectangle(const Vertex &bottomLeft, const Vertex &topRight);
 
 public:
+    // Constructors
+    Rectangle(const Vertex &bottomLeft, const Vertex &topRight);
+    Rectangle(const Vertex vertices[2]);
+    Rectangle(double x, double y, double width, double height);
+    Rectangle(const Vertex &center, double width, double height);
 
-  // Constructors
-  Rectangle(const Vertex& bottomLeft, const Vertex& topRight);
-  Rectangle(const Vertex vertices[2]);
-  Rectangle(double x, double y, double width, double height);
-  Rectangle(const Vertex& center, double width, double height);
+    // Rectangle only functions
+    Vertex getBottomLeft() const;
+    Vertex getTopRight() const;
+    double getWidth() const;
+    double getHeight() const;
 
-  // Rectangle only functions
-  Vertex getBottomLeft() const;
-  Vertex getTopRight() const;
-  double getWidth() const;
-  double getHeight() const;
-
-  // All shapes functions
-  void draw(Board& board) const;
-  Rectangle getBoundingRectangle() const;
-  double getPerimeter() const;
-  double getArea() const;
-  Vertex getCenter() const;
-  bool scale(double factor);
-
-  // Rectangle utility functions
-  bool validRectangle(const Vertex& bottomLeft, const Vertex& topRight);
+    // All shapes functions
+    void draw(Board &board) const;
+    Rectangle getBoundingRectangle() const;
+    double getPerimeter() const;
+    double getArea() const;
+    Vertex getCenter() const;
+    bool scale(double factor);
 };
-
-
